2dcb572e7224bbd1893548a8d726c72401b4cc61
Xenos.exe <55803068+XenoS-ITA@users.noreply.github.com>
2022-05-05 22:16:21 +0200

    Update App.js

--- example/react-base/src/App.js
+++ example/react-base/src/App.js
@@ -2,6 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  function OnMessage(event) {
+    console.log("Message received: " + event.data.message)
+  }
+
+  useEffect(() => {
+    window.addEventListener("message", OnMessage)
+
+    return () => {
+      window.removeEventListener("message", OnMessage)
+    }
+  });
+  
+
   return (
     <>
     </>
